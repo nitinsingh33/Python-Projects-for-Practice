542901e49e0d1f2edfd60a7591b507d3f7d19a8b
nitinsingh33 <nitinsingh.iitp@gmail.com>
2024-11-17 22:32:48 +0530

    raw structure

---
+++ dice_rolling_game.py
@@ -0,0 +1,12 @@
+# Working approach on this problem
+#loop
+    # Ask: roll the dice?
+    # if users enter y
+    #   generates two random numbers
+    #   print them 
+    # if users enters n
+    #   print thank you message
+    #   terminate
+    # else
+    #   print invalid choice
+
